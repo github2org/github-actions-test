c4c1e5b043ad14a2103caee84d405d7a8923d132
manikanta478 <107850586+manikanta478@users.noreply.github.com>
2025-09-22 11:31:43 +0530

    Update build_layer.sh

--- build_layer.sh
+++ build_layer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-rm -rf output
-mkdir -p output/python
+mkdir -p python/python/lib/python3.13/site-packages
 
 docker buildx build --platform linux/amd64 \
   --build-arg REQ_FILE=requirements.txt \
@@ -11,12 +10,27 @@ docker buildx build --platform linux/amd64 \
 
 docker run --rm -v "$PWD:/output" \
   --entrypoint /bin/bash \
-  lambda-layer -c "chown -R 1000:1000 /opt/python && cp -r /opt/python/* /output/python/python/lib/python3.13/site-packages/"
+  lambda-layer -c "cp -r /opt/python/* /output/python/python/lib/python3.13/site-packages/"
 
-cd output
-zip -r ../lambda_layer.zip python
+# Remove test folders
+find python/ -type d -iname "tests" -exec rm -rf {} +
+find python/ -type d -iname "test" -exec rm -rf {} +
+
+# Remove __pycache__ folders
+find python/ -type d -name "__pycache__" -exec rm -rf {} +
+
+# Remove *.pyc and *.pyo files
+find python/ -type f \( -name "*.pyc" -o -name "*.pyo" \) -delete
+
+# Remove dist-info metadata if you don’t need it at runtime
+# (optional: can save 10-20 MB)
+find python/ -type d -name "*.dist-info" -exec rm -rf {} +
+
+# Remove *.egg-info if present
+find python/ -type d -name "*.egg-info" -exec rm -rf {} +
+
+echo NOONOANONAOANO
+cd python
+zip -r ../lambda_layer.zip .
 cd ..
-
-rm -rf output/python
-
-echo "Lambda layer zip created."
+rm -rf python
